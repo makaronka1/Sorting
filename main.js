73c5f6fd5f8efca65e01f7fe5fc8d88598c27884
unknown <marck.pasha3@gmail.com>
2026-02-03 19:15:10 +0300

    add html and css, rework sorting function

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
-let array = [11, 3, 12, 4, -7, 10, 777, 666, 858];
 
-function bubbleSorting (arr) {
+async function bubbleSorting(arr, containersArray) {
+  if (containersArray && typeof containersArray.forEach === 'function' && !Array.isArray(containersArray)) {
+    containersArray = Array.from(containersArray);
+  }
+  
   let isStopped = false;
+  
   while (!isStopped) {
-    for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > arr[i + 1]) {
-      buffer = arr[i + 1];
-      arr[i + 1] = arr[i];
-      arr[i] = buffer;
-      if (isSorted(arr)) {
-        isStopped = true;
+    for (let i = 0; i < arr.length - 1; i++) {
+      
+      if (arr[i] > arr[i + 1]) {
+        const savedValue = arr[i + 1];
+        const savedContainer = containersArray[i + 1];
+        const savedOrder = containersArray[i + 1].style.order;
+        
+        arr[i + 1] = arr[i];
+        arr[i] = savedValue;
+        
+        const currentOrder = containersArray[i].style.order;
+        containersArray[i].style.order = savedOrder;
+        containersArray[i + 1].style.order = currentOrder;
+        
+        containersArray[i + 1] = containersArray[i];
+        containersArray[i] = savedContainer;
+        
+        if (isSorted(arr)) {
+          isStopped = true;
+        }
+        
+        await new Promise(resolve => setTimeout(resolve, 1000));
       }
     }
-   }
   }
+  
   return arr;
 }
 
@@ -26,4 +45,32 @@ function isSorted (arr) {
   return true;
 }
 
-console.log(bubbleSorting(array));
+function gettingArray () {
+  let array = [];
+  let values = document.querySelectorAll('.value-number');
+  for (let value of values) {
+    let textValue = value.textContent;
+    array.push(Number(textValue));
+  }
+  return array;
+}
+
+function setStartOrder () {
+  let valuesContainers = document.querySelectorAll('.value');
+
+  for (let i = 0; i < valuesContainers.length; i++) {
+    valuesContainers[i].style.order = i;
+  }
+
+  return valuesContainers;
+}
+
+function startSorting () {
+  let valuesContainers = setStartOrder();
+  let array = gettingArray();
+  bubbleSorting(array, valuesContainers);
+}
+
+let sortBtn = document.querySelector('.button');
+sortBtn.addEventListener('click', startSorting);
+
