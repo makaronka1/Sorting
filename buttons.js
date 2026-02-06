c1a9c297167727133fa2d0c802c1a376be2a7555
unknown <marck.pasha3@gmail.com>
2026-02-06 22:46:58 +0300

    Добавлены кнопки (стрелочки) для переключения видов сортировки

--- buttons.js
+++ buttons.js
@@ -1,11 +1,16 @@
 const sortBtn = document.querySelector('.sort-button');
 const resetBtn = document.querySelector('.reset-button');
+const arrowBtns = document.querySelectorAll('.arrow-container');
+
+const sortType = ['Bubble sort', 'Selection sort'];
 
 toggleVisibilityBtn(resetBtn);
 
 async function startSort () {
   const sortType = getSortMethod();
   toggleVisibilityBtn(sortBtn);
+  toggleVisibilityBtn(arrowBtns[0]);
+  toggleVisibilityBtn(arrowBtns[1]);
   setValuesFromInput();
   toggleVisibilityInputs();
   toggleVisibilityValues();
@@ -29,7 +34,32 @@ function reset () {
   clearInputs();
   toggleVisibilityBtn(resetBtn);
   toggleVisibilityBtn(sortBtn);
+  toggleVisibilityBtn(arrowBtns[0]);
+  toggleVisibilityBtn(arrowBtns[1]);
 }
+
+function changeSortType (e) {
+  const target = e.currentTarget;
+  const currentSortMethod = getSortMethod();
+  const currentSortIndex = sortType.indexOf(currentSortMethod);
+
+  if (target.classList.contains('left')) {
+    if (currentSortIndex != 0) {
+      document.querySelector('h1').textContent = sortType[currentSortIndex - 1];
+    } else {
+      document.querySelector('h1').textContent = sortType[sortType.length - 1];
+    }
+  } else if (target.classList.contains('right')) {
+    if (currentSortIndex != sortType.length - 1) {
+      document.querySelector('h1').textContent = sortType[currentSortIndex + 1];
+    } else {
+      document.querySelector('h1').textContent = sortType[0];
+    }
+  }
+}
+
+arrowBtns[0].addEventListener('click', changeSortType);
+arrowBtns[1].addEventListener('click', changeSortType);
 
 resetBtn.addEventListener('click', reset);
 sortBtn.addEventListener('click', startSort);
